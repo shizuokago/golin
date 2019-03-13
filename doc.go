a46cb8fc9511eba52529181d7dd6daf949a5db2f
蛇嶋 雄三 <z000235@yunit.vic>
2019-03-14 07:25:18 +0900

    docs : Normalization
    Document的にバグがあったので修正

--- doc.go
+++ doc.go
@@ -1,12 +1,10 @@
-//
 // Golin is Switching the symbolic link of GOROOT
 //
-// んなもんDockerでやりゃいい！という思いを跳ね除け、
-// Shizuoka.goの為に作りましたが、多分secondarykeyはそのままつかいます
+// そんなことDockerでやりゃいい！という思いを跳ね除け、
+// Shizuoka.goの為に作りました。
+// パッケージ構成等の勉強も兼ねてパッケージ化を行って
+// 何個かをAPI化しています
+//
 // https://github.com/shizuokago/golin で管理しています
 //
-// versionが対象ディレクトリに存在しない場合、自動的にダウンロードを行い、
-// バージョンの切り替えを行ってくれます
-//
-
 package golin
